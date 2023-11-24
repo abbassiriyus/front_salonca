3fbe74c04210497f43ae449c93f187f638b6987c
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-11-24 17:24:56 +0500

    2121
    2121212

--- src/pages/js/Calendar_page.jsx
+++ src/pages/js/Calendar_page.jsx
@@ -4,25 +4,68 @@ import ReactDOM from "react-dom";
 import {Calendar,momentLocalizer,setLocalizer,Views} from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-let allViews = Object.keys(Views).map((k) => Views[k]);
+import axios from "axios";
+import url from "../config/host";
+import { useEffect } from "react";
+let allViews = Object.keys(Views).map((a) => Views[a]);
 
 var localizer=momentLocalizer(moment);
 export default function Calendar_page() {
 const [selected, setSelected] = useState();
-  const events = [
-    {
-      id: 0,
-      name: "Holiday",
-      description: "this is description",
-      allDay: true,
-      start: new Date(2022, 9, 7),
-      end: new Date(2022, 9, 7)
-    }
-  ];
+const [data,setData]=useState([])
+
+
+  useEffect(()=>{
+    axios.get(`${url}/api/zakaz`).then(res=>{
+        const Filter=res.data.filter(item=>item.user_id==JSON.parse(localStorage.getItem("one_user"))[0].id)
+        Filter.map(zakaz=>{
+         for (let i = 0; i < Filter.length; i++) {
+           var dateE=new Date((Filter[i].day_zakaz).slice(0,4),(Filter[i].day_zakaz).slice(5,7)-1,(Filter[i].day_zakaz).slice(8,10),(Filter[i].time_end).slice(0,2),(Filter[i].time_end).slice(3,5))
+           var dateB=new Date((Filter[i].day_zakaz).slice(0,4),(Filter[i].day_zakaz).slice(5,7)-1,(Filter[i].day_zakaz).slice(8,10),(Filter[i].time_start).slice(0,2),(Filter[i].time_start).slice(3,5))
+           Filter[i].description=Filter[i].message
+           Filter[i].start=dateB
+           Filter[i].end=dateE
+           Filter[i].allDay=false
+         }
+        })
+        axios.get(`${url}/api/mutahasis`).then(res1=>{
+            axios.get(`${url}/api/filyal`).then(res2=>{
+                for (let j = 0; j < Filter.length; j++) {
+                    for (let d = 0; d < res1.data.length; d++) {
+                      if(Filter[j].mutahasis_id==res1.data[d].id){
+                        Filter[j].filial_id=res1.data[d].filial_id
+                      }
+                    }
+                  }
+                  for (let i = 0; i < Filter.length; i++) {
+                    for (let n = 0; n < res2.data.length; n++) {
+                      if(Filter[i].filial_id==res2.data[n].id){
+                        Filter[i].title=res2.data[n].name
+                       }
+                    }
+                  }
+                  setData(Filter)
+                  
+            })
+        })
+    })
+  },[])
+//   const events = [
+//     {
+//       id: 0,
+//       name: "Holiday",
+//       description: "this is description",
+//       allDay: true,
+//       start: new Date(2022, 9, 7),
+//       end: new Date(2022, 9, 7)
+//     }
+//   ];
+
+  const events = data
   const event = ({ event }) => {
     return (
       <div>
-        {event.name} <br /> <small>{event.description}</small>{" "}
+        {event.title} <br /> <small>{event.description}</small>{" "}
       </div>
     );
   };
@@ -40,7 +83,7 @@ const [selected, setSelected] = useState();
         step={60}
         showMultiDayTimes
         
-        defaultDate={new Date(2022, 9, 7)}
+        defaultDate={new Date()}
         style={{ minHeight: 580 }}
         components={{
           event: event
--- src/pages/js/User_Page.jsx
+++ src/pages/js/User_Page.jsx
@@ -3,9 +3,55 @@ import Navbar_page from "./Navbar_page";
 import Footer_page from "./Footer_page";
 import s from "../css/User_page.module.css" 
 import Calendar_page from "./Calendar_page"
+import axios from "axios";
+import url from "../config/host";
 export default class User_Page extends Component {
 
+  state={
+    category:[],
+    passwordd:[],
+    superAdmin:[],
+  }
+  
 
+   componentDidMount(){
+    axios.get(`${url}/api/users`).then(res=>{
+      const Filter=res.data.filter(item=>item.id==JSON.parse(localStorage.getItem("one_user"))[0].id)
+      axios.get(`${url}/api/category`).then(cat=>{
+            this.setState({category:cat.data})
+      })
+      Filter.map(item=>{
+        this.setState({passwordd:item.password})
+        this.setState({superAdmin:item.superadmin})
+        document.querySelector("#user_email").value=item.email
+        document.querySelector("#user_name").value=item.username
+        document.querySelector("#user_phonenumber").value=item.phone
+        document.querySelector("#user_category").value=item.category
+          
+      })
+      })
+   }
+
+   UserPut=()=>{
+    var formdata=new FormData()
+    formdata.append("phone",document.querySelector("#user_phonenumber").value)
+    formdata.append("password",this.state.passwordd)
+    formdata.append("username",document.querySelector("#user_name").value)
+    formdata.append("superadmin",this.state.superAdmin)
+    formdata.append("email",document.querySelector("#user_email").value)
+    formdata.append("category",document.querySelector("#user_category").value)
+
+    axios.put(`${url}/api/users/${JSON.parse(localStorage.getItem("one_user"))[0].id}`,formdata).then(res=>{
+    alert("Your information has changed.")
+    window.location.reload()
+    }).catch(err=>{
+      alert("Your information has not changed.")
+    })
+   }
+   Exit=()=>{
+    localStorage.clear()
+    window.location="/"
+   }
   
   render() {
     return <div>
@@ -15,8 +61,28 @@ export default class User_Page extends Component {
 <Calendar_page/>
   </div>
   <div className={s.form}>
-asa
-
+   <div className={s.form_title}><h1>Personal data</h1><button onClick={()=>this.Exit()}>Go Out</button></div>
+   <label htmlFor="">
+    <p>Email</p>
+    <input id="user_email" type="text" disabled/>
+   </label>
+   <label htmlFor="">
+    <p>Name</p>
+    <input id="user_name" type="text" />
+   </label>
+   <label htmlFor="">
+    <p>Phone number</p>
+    <input id="user_phonenumber" type="text" />
+   </label>
+   <label htmlFor="">
+    <p>Specialization</p>
+    <select id="user_category">
+      {this.state.category.map(item=>{
+      return <option value={item.id}>{item.category}</option>
+      })}
+    </select>
+    <button onClick={()=>this.UserPut()} className={s.form_button}>Save</button>
+   </label>
   </div>
 </div>
 <Footer_page/>
